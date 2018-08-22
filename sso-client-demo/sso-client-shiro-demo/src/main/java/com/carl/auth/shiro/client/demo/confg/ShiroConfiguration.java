d44f2000d91272c5a210b6791e0452b8d7fce6a4
kenny <kangning.liu@brisky.cc>
2018-08-22 14:16:06 +0800

    添加jwt认证方式

--- sso-client-demo/sso-client-shiro-demo/src/main/java/com/carl/auth/shiro/client/demo/confg/ShiroConfiguration.java
+++ sso-client-demo/sso-client-shiro-demo/src/main/java/com/carl/auth/shiro/client/demo/confg/ShiroConfiguration.java
@@ -10,6 +10,7 @@ import io.buji.pac4j.filter.LogoutFilter;
 import io.buji.pac4j.filter.SecurityFilter;
 import io.buji.pac4j.subject.Pac4jSubjectFactory;
 import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.spring.web.config.AbstractShiroWebFilterConfiguration;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
@@ -20,6 +21,11 @@ import org.pac4j.cas.config.CasConfiguration;
 import org.pac4j.cas.config.CasProtocol;
 import org.pac4j.core.client.Clients;
 import org.pac4j.core.config.Config;
+import org.pac4j.http.client.direct.ParameterClient;
+import org.pac4j.jwt.config.encryption.SecretEncryptionConfiguration;
+import org.pac4j.jwt.config.signature.SecretSignatureConfiguration;
+import org.pac4j.jwt.credentials.authenticator.JwtAuthenticator;
+import org.pac4j.jwt.profile.JwtGenerator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -46,7 +52,9 @@ public class ShiroConfiguration extends AbstractShiroWebFilterConfiguration {
     @Value("#{ @environment['cas.serviceUrl'] ?: null }")
     private String serviceUrl;
 
-
+    //jwt秘钥
+    @Value("${jwt.salt}")
+    private String salt;
     /**
      * cas核心过滤器，把支持的client写上，filter过滤时才会处理，clients必须在casConfig.clients已经注册
      *
@@ -55,11 +63,19 @@ public class ShiroConfiguration extends AbstractShiroWebFilterConfiguration {
     @Bean
     public Filter casSecurityFilter() {
         SecurityFilter filter = new SecurityFilter();
-        filter.setClients("cas,rest");
+        filter.setClients("cas,rest,jwt");
         filter.setConfig(casConfig());
         return filter;
     }
 
+    /**
+     * JWT Token 生成器，对CommonProfile生成然后每次携带token访问
+     * @return
+     */
+    @Bean
+    protected JwtGenerator jwtGenerator() {
+        return new JwtGenerator(new SecretSignatureConfiguration(salt), new SecretEncryptionConfiguration(salt));
+    }
 
 
     /**
@@ -79,9 +95,28 @@ public class ShiroConfiguration extends AbstractShiroWebFilterConfiguration {
     protected Clients clients() {
         //可以设置默认client
         Clients clients = new Clients();
+
+        //token校验器，可以用HeaderClient更安全
+        ParameterClient parameterClient = new ParameterClient("token", jwtAuthenticator());
+        parameterClient.setSupportGetRequest(true);
+        parameterClient.setName("jwt");
+
         //支持的client全部设置进去
-        clients.setClients(casClient(), casRestFormClient());
+        clients.setClients(casClient(), casRestFormClient(),parameterClient);
         return clients;
+    }
+
+
+    /**
+     * JWT校验器，也就是目前设置的ParameterClient进行的校验器，是rest/或者前后端分离的核心校验器
+     * @return
+     */
+    @Bean
+    protected JwtAuthenticator jwtAuthenticator() {
+        JwtAuthenticator jwtAuthenticator = new JwtAuthenticator();
+        jwtAuthenticator.addSignatureConfiguration(new SecretSignatureConfiguration(salt));
+        jwtAuthenticator.addEncryptionConfiguration(new SecretEncryptionConfiguration(salt));
+        return jwtAuthenticator;
     }
 
 
@@ -135,12 +170,13 @@ public class ShiroConfiguration extends AbstractShiroWebFilterConfiguration {
      * @return
      */
     @Bean
-    protected ShiroFilterFactoryBean shiroFilterFactoryBean() {
+    protected ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
         //把subject对象设为subjectFactory
         //由于cas代理了用户，所以必须通过cas进行创建对象
         ((DefaultSecurityManager) securityManager).setSubjectFactory(new Pac4jSubjectFactory());
 
         ShiroFilterFactoryBean filterFactoryBean = super.shiroFilterFactoryBean();
+        filterFactoryBean.setSecurityManager(securityManager);
         filterFactoryBean.setFilters(shiroFilters());
         return filterFactoryBean;
     }
